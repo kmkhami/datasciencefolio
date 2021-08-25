1152d54e581052535f4fd99a9f88b1895fc3aca3
kmkhami <nicholas.khami@gmail.com>
2021-08-25 00:20:51 -0500

    portfolio.js update information

--- src/portfolio.js
+++ src/portfolio.js
@@ -225,49 +225,48 @@ const projectsHeader = {
 const projectcards = {
   list: [
     {
-      title: "TEATH",
-      img_path: "project-06.png",
+      title: "Optimal Well Location Selector",
+      img_path: "RF.gif",
       description:
-        "A WebApp for Extracting Text from Images and applying various thresholding methods with sentiment analysis using TextBlob",
+        "A project that used the Python Data Stack to create a ML algorithm that would select optimal well locations in a theoretical basin.",
       tags: [
         {
           lang: "python",
           color: "#004782",
         },
         {
-          lang: "TextBlob",
+          lang: "PyTorch",
+          color: "#004782",
+        },
+        {
+          lang: "RandomForest",
           color: "#820032",
         },
         {
-          lang: "OpenCV",
+          lang: "conocophillips",
           color: "#00a100",
         },
         {
-          lang: "Streamlit",
+          lang: "Jupyter",
           color: "#ff4b4b",
-        },
-        {
-          lang: "Heroku",
-          color: "#3c0095",
-        },
+        }
       ],
-      link: "https://teath.herokuapp.com ",
-      code: "https://github.com/rohankokkula/TEATH",
+      link: "https://github.com/kmkhami/Well_Location_Selection",
+      code: "https://github.com/kmkhami/Well_Location_Selection",
       linkcolor: "white",
     },
-
     {
-      title: "Voice Gender Detection",
-      img_path: "voice.gif",
+      title: "Automatic Feature Highlighting In Noisy RES Data",
+      img_path: "gan.gif",
       description:
-        "A streamlit webapp which detects user's gender and emotion based on realtime audio input.",
+        "First author on a Computer Vision research paper that uses CycleGAN for denoising.",
       tags: [
         {
-          lang: "ongoing",
+          lang: "python",
           color: "red",
         },
         {
-          lang: "python",
+          lang: "CycleGAN",
           color: "#004782",
         },
         {
@@ -275,38 +274,38 @@ const projectcards = {
           color: "#c47206",
         },
         {
-          lang: "tensorflow",
+          lang: "PyTorch",
           color: "#4257f5",
         },
         {
-          lang: "Streamlit",
+          lang: "Jupyter",
           color: "#ff4b4b",
         },
       ],
-      link: "",
-      code: "",
+      link: "https://www.nicholaskhami.com/documents/Feature_Highlighting_Prepublication.pdf",
+      code: "https://www.nicholaskhami.com/documents/Feature_Highlighting_Prepublication.pdf",
       linkcolor: "white",
     },
     {
-      title: "NeuralViz",
-      img_path: "neural.gif",
+      title: "RapiDocs",
+      img_path: "gan.gif",
       description:
-        "A webapp to visualize Neural Networks based on no. of layers and neurons, calculate Loss and understand math behind Back Propagation algo.",
+        "A webapp to allow patients to take control of their EHR data.",
       tags: [
         {
-          lang: "python",
+          lang: "React.JS",
           color: "#004782",
         },
         {
-          lang: "numpy",
+          lang: "HTML5",
           color: "#8700b0",
         },
         {
-          lang: "matplotlib",
+          lang: "CSS3",
           color: "#c47206",
         },
         {
-          lang: "Streamlit",
+          lang: "Firebase",
           color: "#ff4b4b",
         },
         {
@@ -314,103 +313,10 @@ const projectcards = {
           color: "#3c0095",
         },
       ],
-      link: "https://neuralviz.herokuapp.com/",
-      code: "https://github.com/rohankokkula/neuralviz",
+      link: "https://devpost.com/software/rapidocs",
+      code: "https://github.com/Abinavraj5427/rapidocs",
       linkcolor: "white",
-    },
-    {
-      title: "Semantic Search APP",
-      img_path: "project-03.png",
-      description:
-        "Demo of OpenAI's GPT-3 model on Semantic search using streamlit.",
-      tags: [
-        {
-          lang: "ongoing",
-          color: "red",
-        },
-        {
-          lang: "Streamlit",
-          color: "#ff4b4b",
-        },
-      ],
-      link: "",
-      code: "",
-      linkcolor: "white",
-    },
-    {
-      title: "Youtube Thumbnail Generator & AutoUploader",
-      img_path: "project-02.png",
-      description:
-        "A python streamlit webapp to automate generating & uploading youtube thumbnails using Youtube v3 API",
-      tags: [
-        {
-          lang: "internal project",
-          color: "red",
-        },
-        {
-          lang: "youtube v3 api",
-          color: "red",
-        },
-      ],
-      link: "",
-      code: "",
-      linkcolor: "white",
-    },
-
-    {
-      title: "Newsearcher",
-      img_path: "news.gif",
-      description:
-        "Enter your search keyword and get a summary of news weblinks with images without actually opening the links.",
-      tags: [
-        {
-          lang: "python",
-          color: "#004782",
-        },
-        {
-          lang: "newspaper3k",
-          color: "#303030",
-        },
-        {
-          lang: "Streamlit",
-          color: "#ff4b4b",
-        },
-        {
-          lang: "nltk",
-          color: "#051c61",
-        },
-      ],
-      link: "https://newsearcher.herokuapp.com/",
-      code: "https://github.com/rohankokkula/Newsearcher",
-      linkcolor: "white",
-    },
-    {
-      title: "ActiWeight",
-      img_path: "acti.gif",
-      description:
-        "ActiWeight is an Interactive Visualizer for understanding intuition behind 10 Activation Functions used in Neural Networks.",
-      tags: [
-        {
-          lang: "python",
-          color: "#004782",
-        },
-        {
-          lang: "matplotlib",
-          color: "#c47206",
-        },
-        {
-          lang: "Streamlit",
-          color: "#ff4b4b",
-        },
-        {
-          lang: "Heroku",
-          color: "#3c0095",
-        },
-      ],
-      link: "https://www.youtube.com/watch?v=2vk9lQuX_1Q",
-      code: "https://github.com/rohankokkula/Actiweight",
-      linkcolor: "white",
-    },
+    }
   ],
 };
 
@@ -419,50 +325,27 @@ const resumeleft = {
     title: ["Experience"],
     data: [
       {
-        title: "Data Scientist",
-        subtitle: "Nactus India Services Private Limited",
-        date: "June 2020 - present",
+        title: "Undergraduate Data Science Researcher",
+        subtitle: "ConocoPhillips and UT Austin",
+        date: "August 2020 - May 2021",
         content: [
-          "Used T5 Transformer Model to build a Question Answering Model which generates questions based on Keywords extracted (WordNet, Sense2Vec) from studyshot notes.",
-          "Built an MCQ Generation model using BERT",
-          "Developed a personalized Chatbot using Rasa (Botfront.io).",
-          "Created an Automatic Thumbnail Generator & Uploader for our youtube videos using Youtube v3 API",
-          "Working on OpenAI's semantic search engine.",
-          "Created a Clean MaterialUI Design for prototyping and design fixing before production-ready of APP",
-          "Created FrontEnd components for StudyShot website",
-          "Working on recommendation system for concept notes and questions based on student's behavior on our Flutter App(tracking)",
+          "Utilized the Python data stack to to perform statistcal analysis on geological survey data.",
+          "Communicated research observations through markdown documentation in Jupyter notebooks and slidedeck presentations.",
+          "Deployed a RandomForestRegressor to make suggestions about the best well locations in a given geological area.",
+          "Used CycleGAN to develop a new technique for denoising and isolating layers in Radio-Echo-Sounding data that is currently in pre-publication."
         ],
       },
       {
-        title: "Machine Learning Intern",
-        subtitle: "Listnr",
-        date: "April 2020 - June 2020",
+        title: "Software Engineering Intern",
+        subtitle: "NelNet PaymentSpring",
+        date: "August 2020 - Current",
         content: [
-          "My role at Listnr included a lot of research work about Audio Encoders and Vocoders and Speech Synthesis in general.",
-          "Worked on some SOTA Inference TTS Models such as TacoTron2, FastSpeech2, and Wavenet.",
-          "Also had hands-on with real-time voice cloning algorithm.",
+          "Managed docker containers and configuration for multi-tier development environment.",
+          "Tested and deployed code onto AWS EC2 instances.",
+          "Worked with PostgresSQL database and related back-end technologies.",
+          "Wrote back-end code in Ruby for a Ruby on Rails server-side web application framework."
         ],
-      },
-      {
-        title: "Data Science Intern",
-        subtitle: "iNeuron.ai",
-        date: "Nov 2020 - January 2021",
-        content: [
-          "Phase 1 included Data Cleaning & Data Annotation for object detection task",
-          "Used YOLO v5 for Fire Detection Project",
-        ],
-      },
-      {
-        title: "Summer Analytics Participant",
-        subtitle: "IIT Guwahati",
-        date: "April 2020 - June 2020",
-        content: [
-          "Learnt & applied various Machine Learning libraries",
-          "Had hands-on with Data Visualization techniques using matplotlib, plotly, streamlit",
-          "Understood and applied Neural networks using Tensorflow and finally competed in Kaggle Hackathon ranking 135 (top 16 percentile) among 854 Kaggle participants",
-          "Ranked 85 out of 1500+ students for overall performance.",
-        ],
-      },
+      }
     ],
   },
 };
@@ -473,172 +356,133 @@ const resumeright = {
       title: ["Education"],
       data: [
         {
-          title: "Bachelor of Engineering in Information Technology",
-          subtitle: "Vidyalankar Institute of Technology, Mumbai",
-          date: "2016 - 2020",
-          content: ["CGPA: 8.06/10"],
+          title: "Bachelor of Computer Science",
+          subtitle: "University of Texas at Austin",
+          date: "2020 - 2024",
+          content: ["GPA: 3.96"]
         },
         {
-          title: "Higher Secondary Certificate [HSC]",
-          subtitle: "Dr. Antonio Da Silva Technical Jr. College, Mumbai",
-          date: "2014 - 2016",
-          content: ["Precentage: 80%"],
+          title: "Bachelor of Applied Mathematics",
+          subtitle: "University of Texas at Austin",
+          date: "2020 - 2024",
+          content: ["GPA: 3.96"]
         },
       ],
-    },
-    {
-      title: ["Ranks and Achivements"],
-      data: [
-        {
-          title: "Ranks",
-          content: [
-            "#135/854 in Summer Analytics Kaggle Competition by IITG",
-            "#113/5064 in CV-NLP Hackathon by HackerEarth",
-          ],
-        },
-        {
-          title: "Achievements",
-          content: [
-            "Designing Team Head at ACM-VIT",
-            "5⭐ Gold Badges for Python & Problem Solving at HackerRank ",
-            "Tableau Analyst & Consumer Badge by Tableau Official",
-          ],
-        },
-      ],
-    },
-    {
-      title: ["Freelance Work"],
-      data: [
-        {
-          title: "Consultancy Website",
-          subtitle: "kidsworldeducationconsultants.com",
-          content: [
-            "Created and hosted a website for consultancy providers in Kids Education Domain",
-          ],
-        },
-        {
-          title: "Portfolio Website",
-          subtitle: "pranavphulware.netlify.app",
-          content: [
-            "Built a personal Filmography portfolio for a friend and later hosted it at Netlify",
-          ],
-        },
-      ],
-    },
+    }
   ],
 };
 
 //certificate cards
-const certifications = {
-  certifications: [
-    {
-      title: "Tableau Analyst",
-      subtitle: "Tableau",
-      logo_path: "tabana-01.png",
-      certificate_link:
-        "https://www.credly.com/badges/2d43f401-7be0-407d-9a77-7477dda8cade/linked_in_profile",
-      alt_name: "Tableau",
-      color_code: "#000000",
-    },
-    {
-      title: "Tableau Consumer",
-      subtitle: "Tableau",
-      logo_path: "tabcon-01.png",
-      certificate_link:
-        "https://www.credly.com/badges/e5befe7b-2eec-486f-b77e-bd2458c18b26/linked_in_profile",
-      alt_name: "Tableau",
-      color_code: "#000000",
-    },
-    {
-      title: "Problem solving",
-      subtitle: "HackerRank",
-      logo_path: "problem-01.png",
-      certificate_link: "https://www.hackerrank.com/rohankokkula",
-      alt_name: "HackerRank",
-      color_code: "#000000",
-    },
-    {
-      title: "Python",
-      subtitle: "HackerRank",
-      logo_path: "python-01.png",
-      certificate_link: "https://www.hackerrank.com/rohankokkula",
-      alt_name: "HackerRank",
-      color_code: "#000000",
-    },
-    {
-      title: "Certificate of Merit",
-      subtitle: "Summer Analytics | IIT Guwahati",
-      logo_path: "iitg-01.png",
-      certificate_link:
-        "https://www.coursera.org/account/accomplishments/verify/22MTSSC5WDTM",
-      alt_name: "IITG",
-      color_code: "#000000",
-    },
-    {
-      title: "Tableau Desktop Qualified Associate",
-      subtitle: "SimpliLearn",
-      logo_path: "simplilearn-01.png",
-      certificate_link:
-        "https://www.coursera.org/account/accomplishments/verify/22MTSSC5WDTM",
-      alt_name: "Simplilearn",
-      color_code: "#000000",
-    },
-    {
-      title: "Question Generation using Transformers",
-      subtitle: "Udemy",
-      logo_path: "udemy-01.png",
-      certificate_link:
-        "https://www.udemy.com/certificate/UC-b17c4c21-91e3-4f8b-b9c2-259468910985/",
-      alt_name: "Udemy",
-      color_code: "#000000",
-    },
-    {
-      title: "Machine Learning",
-      subtitle: "Stanford University",
-      logo_path: "coursera-01.png",
-      certificate_link:
-        "https://www.coursera.org/account/accomplishments/verify/22MTSSC5WDTM",
-      alt_name: "Stanford University",
-      color_code: "#000000",
-    },
-    {
-      title: "Data Visualization",
-      subtitle: "University of Michigan",
-      logo_path: "coursera-01.png",
-      certificate_link:
-        "https://www.coursera.org/account/accomplishments/verify/22MTSSC5WDTM",
-      alt_name: "University of Michigan",
-      color_code: "#000000",
-    },
-    {
-      title: "Data Scientist Career Track",
-      subtitle: "DataCamp",
-      logo_path: "datacamp-01.png",
-      certificate_link:
-        "https://www.datacamp.com/statement-of-accomplishment/track/29f2ddab3fbfd0a470713a19c729c0f9d89c88aa",
-      alt_name: "DataCamp",
-      color_code: "#000000",
-    },
-    {
-      title: "PowerBI Masters",
-      subtitle: "iNeuron.ai",
-      logo_path: "ineuron-01.png",
-      certificate_link:
-        "https://academy.ineuron.ai/certificates/downloads/PB009929.pdf",
-      alt_name: "HackerRank",
-      color_code: "#000000",
-    },
-    {
-      title: "Tableau Masters",
-      subtitle: "iNeuron.ai",
-      logo_path: "ineuron-01.png",
-      certificate_link:
-        "https://academy.ineuron.ai/certificates/downloads/TB000634.pdf",
-      alt_name: "HackerRank",
-      color_code: "#000000",
-    },
-  ],
-};
+// const certifications = {
+//   certifications: [
+//     {
+//       title: "Tableau Analyst",
+//       subtitle: "Tableau",
+//       logo_path: "tabana-01.png",
+//       certificate_link:
+//         "https://www.credly.com/badges/2d43f401-7be0-407d-9a77-7477dda8cade/linked_in_profile",
+//       alt_name: "Tableau",
+//       color_code: "#000000",
+//     },
+//     {
+//       title: "Tableau Consumer",
+//       subtitle: "Tableau",
+//       logo_path: "tabcon-01.png",
+//       certificate_link:
+//         "https://www.credly.com/badges/e5befe7b-2eec-486f-b77e-bd2458c18b26/linked_in_profile",
+//       alt_name: "Tableau",
+//       color_code: "#000000",
+//     },
+//     {
+//       title: "Problem solving",
+//       subtitle: "HackerRank",
+//       logo_path: "problem-01.png",
+//       certificate_link: "https://www.hackerrank.com/rohankokkula",
+//       alt_name: "HackerRank",
+//       color_code: "#000000",
+//     },
+//     {
+//       title: "Python",
+//       subtitle: "HackerRank",
+//       logo_path: "python-01.png",
+//       certificate_link: "https://www.hackerrank.com/rohankokkula",
+//       alt_name: "HackerRank",
+//       color_code: "#000000",
+//     },
+//     {
+//       title: "Certificate of Merit",
+//       subtitle: "Summer Analytics | IIT Guwahati",
+//       logo_path: "iitg-01.png",
+//       certificate_link:
+//         "https://www.coursera.org/account/accomplishments/verify/22MTSSC5WDTM",
+//       alt_name: "IITG",
+//       color_code: "#000000",
+//     },
+//     {
+//       title: "Tableau Desktop Qualified Associate",
+//       subtitle: "SimpliLearn",
+//       logo_path: "simplilearn-01.png",
+//       certificate_link:
+//         "https://www.coursera.org/account/accomplishments/verify/22MTSSC5WDTM",
+//       alt_name: "Simplilearn",
+//       color_code: "#000000",
+//     },
+//     {
+//       title: "Question Generation using Transformers",
+//       subtitle: "Udemy",
+//       logo_path: "udemy-01.png",
+//       certificate_link:
+//         "https://www.udemy.com/certificate/UC-b17c4c21-91e3-4f8b-b9c2-259468910985/",
+//       alt_name: "Udemy",
+//       color_code: "#000000",
+//     },
+//     {
+//       title: "Machine Learning",
+//       subtitle: "Stanford University",
+//       logo_path: "coursera-01.png",
+//       certificate_link:
+//         "https://www.coursera.org/account/accomplishments/verify/22MTSSC5WDTM",
+//       alt_name: "Stanford University",
+//       color_code: "#000000",
+//     },
+//     {
+//       title: "Data Visualization",
+//       subtitle: "University of Michigan",
+//       logo_path: "coursera-01.png",
+//       certificate_link:
+//         "https://www.coursera.org/account/accomplishments/verify/22MTSSC5WDTM",
+//       alt_name: "University of Michigan",
+//       color_code: "#000000",
+//     },
+//     {
+//       title: "Data Scientist Career Track",
+//       subtitle: "DataCamp",
+//       logo_path: "datacamp-01.png",
+//       certificate_link:
+//         "https://www.datacamp.com/statement-of-accomplishment/track/29f2ddab3fbfd0a470713a19c729c0f9d89c88aa",
+//       alt_name: "DataCamp",
+//       color_code: "#000000",
+//     },
+//     {
+//       title: "PowerBI Masters",
+//       subtitle: "iNeuron.ai",
+//       logo_path: "ineuron-01.png",
+//       certificate_link:
+//         "https://academy.ineuron.ai/certificates/downloads/PB009929.pdf",
+//       alt_name: "HackerRank",
+//       color_code: "#000000",
+//     },
+//     {
+//       title: "Tableau Masters",
+//       subtitle: "iNeuron.ai",
+//       logo_path: "ineuron-01.png",
+//       certificate_link:
+//         "https://academy.ineuron.ai/certificates/downloads/TB000634.pdf",
+//       alt_name: "HackerRank",
+//       color_code: "#000000",
+//     },
+//   ],
+// };
 
 // Contact Page
 const contactPageData = {
@@ -691,7 +535,6 @@ export {
   projectsHeader,
   contactPageData,
   projectcards,
-  certifications,
   resumeleft,
   resumeright,
 };
